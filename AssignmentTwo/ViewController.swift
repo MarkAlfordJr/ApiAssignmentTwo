1379cc83ce227008ac3c2640b957c1e9754d2472
Mark Alford Jr <markalfordjr@gmail.com>
2021-07-22 18:15:13 -0400

    Initial Commit

---
+++ AssignmentTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentTwo
+//
+//  Created by Mark Alford on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
